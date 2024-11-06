bb57ea5ac2f8f7872485b44fc41bd47129d9e78f
Vamelyre <168769184+Vamelyre@users.noreply.github.com>
2024-11-06 19:32:48 +0400

    Update stacks.cpp

--- lecture_6/stacks.cpp
+++ lecture_6/stacks.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream> 
 #include <stack>
 #include <string>
@@ -85,43 +86,73 @@ int main() {
 // Write a C++ program that reverses a sequence of integers using a stack.
 // Example Input: Sequence {1, 2, 3, 4, 5}
 // Output: Reversed Sequence: 5 4 3 2 1
-
+/*
 int main(){
     stack<int> my_stack;
     int n;
-    cout << "how many values do you want t oadd to your stack?: ";
+
+    cout << "how many values do you want to add?: ";
     cin >> n;
 
-    cout << "What values do you wantto add?: ";
+    cout << "enter the values you want to add to your stack: ";
     for(int i = 0; i < n; i++){
         int value; 
         cin >> value;
         my_stack.push(value);
     }
 
-    cout << "Reverse your stack? (y/n): ";
+    cout << "reverse your stack? (y/n): ";
     string answer; 
     cin >> answer; 
 
-    if(answer == "y"){
-
+    if(answer == "y") {
         stack<int> temp_stack;
 
-        while(!temp_stack.empty()){
-            int temp_value = my_stack.top();
+        while(!my_stack.empty()){
+            temp_stack.push(my_stack.top());
             my_stack.pop();
-            temp_stack.push(temp_value);
         }
 
+        cout << "reversed stakc: ";
         while(!temp_stack.empty()){
             cout << temp_stack.top() << " ";
             temp_stack.pop();
-
         }
-    } else {
+        cout << endl;
+    } 
+    else {
+        cout << "original stack: ";
         while(!my_stack.empty()){
             cout << my_stack.top() << " "; 
             my_stack.pop();
         }
+        cout << endl;
     }
+    return 0;
+
 }
+*/
+
+// Write a C++ program that counts and prints the number of elements in a stack.
+// Example Input: Stack {1, 2, 3, 4, 5}
+// Output: Number of elements in stack: 5
+/*
+int main(){
+    stack<int> my_stack; 
+    int counter = 0;
+
+    for(int i = 6; i > 1; i--){
+        my_stack.push(i);
+    }
+
+    stack<int> temp_stack = my_stack;
+
+    while(!temp_stack.empty()){
+        temp_stack.pop();
+        counter++;
+    }
+
+    cout << "num of elemebts in stack: " << counter << endl;
+    return 0;
+}
+*/
